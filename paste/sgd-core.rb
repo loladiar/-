8f969d150e6b50e7ea5a542fbb7b5f35544a557e
henry4j <henry4js@gmail.com>
2014-01-28 22:18:50 -0800

    .

--- paste/sgd-core.rb
+++ paste/sgd-core.rb
@@ -40,7 +40,7 @@ class SGD
 
   def ngram(terms, max_ngram)
     term_iterator = org.apache.mahout.common.lucene.IteratorTokenStream.new(java.util.Arrays.asList(terms.to_java).iterator)
-    stream  = org.apache.lucene.analysis.shingle.ShingleFilter.new(term_iterator, max_ngram).tap { |e| e.reset }
+    stream = org.apache.lucene.analysis.shingle.ShingleFilter.new(term_iterator, max_ngram).tap { |e| e.reset }
     term_attr = stream.add_attribute(org.apache.lucene.analysis.tokenattributes.CharTermAttribute.java_class)
     tokens = []
     begin
